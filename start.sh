bbcb13b50169df26a836d39dfc007be5d756c312
iirthw <iirthw@gmail.com>
2021-12-11 22:36:33 +0200

    add main executable.

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Currently the script supports only the macOS and Windows
 # i.e. if OS type is not darwin (macOS), then we directly assume windows otherwise
 if [[ $OSTYPE == "darwin"* ]]; then
-        open build/foobar.xcodeproj
+        open build/Takar.xcodeproj
 else
-        start build/foobar.sln
+        start build/Takar.sln
 fi
---
+++ takar/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Takar" << std::endl;
+	return 0;
+} // main
