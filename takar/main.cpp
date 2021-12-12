316a2eb90718b4000d79edc56b93a4281e7462dd
iirthw <iirthw@gmail.com>
2021-12-12 20:27:47 +0200

    simple main impl

--- takar/main.cpp
+++ takar/main.cpp
@@ -1,40 +1,59 @@
+#include <cassert>
 #include <iostream>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 int main() {
 	std::cout << "Takar" << std::endl;
     
-//    GLFWwindow* window;
-//
-//    /* Initialize the library */
-//    if (!glfwInit())
-//        return -1;
-//
-//    /* Create a windowed mode window and its OpenGL context */
-//    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
-//    if (!window)
-//    {
-//        glfwTerminate();
-//        return -1;
-//    }
-//
-//    /* Make the window's context current */
-//    glfwMakeContextCurrent(window);
-//    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
-//
-//    /* Loop until the user closes the window */
-//    while (!glfwWindowShouldClose(window))
-//    {
-//        /* Render here */
-//        glClear(GL_COLOR_BUFFER_BIT);
-//
-//        /* Swap front and back buffers */
-//        glfwSwapBuffers(window);
-//
-//        /* Poll for and process events */
-//        glfwPollEvents();
-//    }
-//
-//    glfwTerminate();
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        std::cout << "[error] glfwInit failed." << std::endl;
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current, which is not done by default
+    // after window creation.
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "[error] Failed to initialize GLAD." << std::endl;
+        assert(false);
+        glfwTerminate();
+        
+        return -1;
+    }
+
+    glClearColor(0.0f, 0.3f, 0.4f, 0.0f);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     
 	return 0;
 } // main
