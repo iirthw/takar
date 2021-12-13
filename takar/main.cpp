730de67866a32b08a6417192f60e0872ffa7347f
iirthw <iirthw@gmail.com>
2021-12-13 18:28:35 +0200

    [glfw] fix macos-specific window hint

--- takar/main.cpp
+++ takar/main.cpp
@@ -18,7 +18,9 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
