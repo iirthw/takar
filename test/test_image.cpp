31ce3c8ab3f9fcb5f48cbb3ae54345b66e9ad717
iirthw <iirthw@gmail.com>
2021-12-21 23:51:50 +0200

    [test] Add test body.

--- test/test_image.cpp
+++ test/test_image.cpp
@@ -1,1 +1,27 @@
+#include "gtest/gtest.h"
+
 #include "image.h"
+
+class TestSuite1 : public ::testing::Test {
+public:
+
+    static std::string workingDir()
+    {
+        static std::string workDir =
+            testing::UnitTest::GetInstance()->original_working_dir();
+
+        return workDir;
+    }
+
+protected:
+
+    void SetUp() override
+    {}
+
+    // void TearDown() override {}
+};
+
+TEST(TestSuite1, TestCtor)
+{
+    ASSERT_TRUE(false);
+}
