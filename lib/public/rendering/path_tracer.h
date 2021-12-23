ae02f318348bfca0ac42ab6299e49f3a35e8d1e4
iirthw <iirthw@gmail.com>
2021-12-24 00:20:42 +0200

    [lib] path_tracer interface

--- lib/public/rendering/path_tracer.h
+++ lib/public/rendering/path_tracer.h
@@ -1,9 +1,19 @@
 #pragma once
 
-namespace tkr
-{
-    class PathTracer
-    {
+#include <memory>
 
+#include "image.h"
+
+namespace tkr {
+    class PathTracer {
+    public:
+        PathTracer(std::shared_ptr<Scene> scene, std::shared_ptr<Camera> camera);
+
+        Image renderToImage() const;
+
+    private:
+        std::shared_ptr<Scene> mScene;
+        std::shared_ptr<Camera> mCamera;
     }; // class PathTracer
+
 } // namespace tkr
