ed24ae224f1992220b670cc0d1d3ee6844c6a5b3
iirthw <iirthw@gmail.com>
2021-12-23 12:08:26 +0200

    [lib] [ray] wip: ray header.

--- lib/public/geometry/ray.h
+++ lib/public/geometry/ray.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include "vec3.h"
+
 namespace tkr
 {
-    class Ray
+    class RayBase
     {
+    public:
+        Ray(const mq::vec3& origin, const mq::vec3& direction);
 
+        mq::vec3 at(float t) const;
+        inline mq::vec3 origin() const { return mOrigin; }
+        inline mq::vec3 direction() const { return mDirection; }
+
+    private:
+
+        mq::vec3 mOrigin;
+        mq::vec3 mDirection;
     }; // class Ray
+
 } // namespace tkr
