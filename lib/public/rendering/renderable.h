8f55647ca89419b16514fbc45a10a0f4e26404a3
iirthw <iirthw@gmail.com>
2021-12-24 00:20:27 +0200

    [lib] renderable interface

--- lib/public/rendering/renderable.h
+++ lib/public/rendering/renderable.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include "rendering/ray.h"
+
 namespace tkr
 {
     class Renderable
     {
-
+    public:
+        virtual bool rayIntersect(const Ray& ray) const = 0;
     }; // class Renderable
 } // namespace tkr
