3c9f1339f291b4f2eb01ba5766651500b9cf1c4e
iirthw <iirthw@gmail.com>
2021-12-24 16:43:39 +0200

    [lib] [rect] extend api

--- lib/impl/geometry/rect.cpp
+++ lib/impl/geometry/rect.cpp
@@ -1,9 +1,13 @@
 #include "geometry/rect.h"
 
+#include "mq.h"
+
 using namespace mq;
 
 namespace tkr
 {
+    Rect::Rect() : mX(0.0f), mY(0.0f), mWidth(0.0f), mHeight(0.0f) {}
+
     Rect::Rect(float x, float y, float width, float height)
     : mX(x)
     , mY(y)
@@ -14,4 +18,14 @@ namespace tkr
     Rect::Rect(const vec2& origin, float width, float height)
     : Rect(origin.x, origin.y, width, height)
     {}
+
+    bool Rect::degenerate() const
+    {
+        return isZero(area());
+    }
+
+    float Rect::area() const
+    {
+        return mWidth * mHeight;
+    }
 } // namespace tkr
--- lib/public/geometry/rect.h
+++ lib/public/geometry/rect.h
@@ -7,10 +7,15 @@ namespace tkr
     class Rect
     {
     public:
+        Rect();
         Rect(float x, float y, float width, float height);
         Rect(const mq::vec2& origin, float width, float height);
 
+        bool degenerate() const;
+        float area() const;
+
     private:
+
         float mX;
         float mY;
         float mWidth;
