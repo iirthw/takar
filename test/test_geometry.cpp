144fbe22674fa5c837e7f9ba5a7dc6c99b332f6f
iirthw <iirthw@gmail.com>
2021-12-24 17:41:53 +0200

    [test] [rect] basic test

--- test/test_geometry.cpp
+++ test/test_geometry.cpp
@@ -1,24 +1,53 @@
 #include "gtest/gtest.h"
 
-class TestGeometry : public ::testing::Test {
-public:
+#include "geometry/rect.h"
+#include "mq.h"
 
-    static std::string workingDir()
-    {
-        static std::string workDir =
-            testing::UnitTest::GetInstance()->original_working_dir();
+using namespace mq;
+using namespace tkr;
 
-        return workDir;
-    }
+namespace tkr {
+    namespace test {
 
-protected:
+        class TestGeometry : public ::testing::Test {
+        public:
 
-    void SetUp() override
-    {}
+            static std::string workingDir()
+            {
+                static std::string workDir =
+                    testing::UnitTest::GetInstance()->original_working_dir();
 
-    // void TearDown() override {}
-};
+                return workDir;
+            }
+
+        protected:
+
+            void SetUp() override
+            {}
+
+            // void TearDown() override {}
+        }; // TestGeometry
+
+    } // namespace test
+} // namespace tkr
 
 TEST(TestRect, TestCtor)
-{    
+{
+    {
+        Rect rect;
+
+        ASSERT_TRUE(rect.degenerate());
+        ASSERT_TRUE(isZero(rect.area()));
+        ASSERT_TRUE(isZero(rect.x()));
+        ASSERT_TRUE(isZero(rect.y()));
+        ASSERT_TRUE(isZero(rect.width()));
+        ASSERT_TRUE(isZero(rect.height()));
+    }
+
+    {
+        Rect rect(0.f, 0.f, 10.f, 2.f);
+
+        ASSERT_FALSE(rect.degenerate());
+        ASSERT_EQ(rect.area(), 20.f);
+    }
 }
