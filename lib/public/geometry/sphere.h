e649cb1a605c3cc8e5411d998611cd93d62db805
iirthw <iirthw@gmail.com>
2021-12-24 18:06:51 +0200

    [lib] sphere basic api

--- lib/public/geometry/sphere.h
+++ lib/public/geometry/sphere.h
@@ -1,9 +1,17 @@
 #pragma once
 
-namespace tkr
-{
-    class Sphere
-    {
+#include "vec3.h"
 
+namespace tkr {
+    class Sphere {
+    public:
+        Sphere(const mq::vec3& center, float radius);
+
+        mq::vec3 center() const { return mCenter; }
+        float radius() const { return mRadius; }
+
+    private:
+        mq::vec3 mCenter;
+        float mRadius;
     }; // class Sphere
 } // namespace tkr
