9c024ac0beabd2751f55c238e0afb0fc2e3f6b3f
iirthw <iirthw@gmail.com>
2021-12-26 20:29:35 +0200

    [lib] wip on renderable

--- lib/impl/rendering/node.cpp
+++ lib/impl/rendering/node.cpp
@@ -1,5 +1,7 @@
 #include "rendering/node.h"
 
 namespace tkr {
-    
+    Node::Node(std::shared_ptr<Renderable> renderable)
+    : mRenderables(std::vector<std::shared_ptr<Renderable>>{renderable})
+    {}
 } // namespace tkr
--- lib/public/rendering/node.h
+++ lib/public/rendering/node.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <memory>
+#include <vector>
+
+#include "rendering/renderable.h"
+
 namespace tkr {
 	class Node {
+	public:
+		Node() = default;
+		Node(std::shared_ptr<Renderable> renderable);
 
+		const std::vector<std::shared_ptr<Renderable>>& 
+			renderables() const { return mRenderables; }
+
+	private:
+		std::vector<std::shared_ptr<Renderable>> mRenderables;
 	}; // class Node
 } // namespace tkr
