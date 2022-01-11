c79aaf35a8e9bb1516427b8858c0ad249a220708
iirthw <iirthw@gmail.com>
2022-01-11 23:35:18 +0200

    [lib] implement Projection::perspective

--- lib/impl/rendering/camera.cpp
+++ lib/impl/rendering/camera.cpp
@@ -6,10 +6,36 @@
 using namespace mq;
 
 namespace tkr {
+    // =========================================================================
+
     Frustum::Frustum(float fov, float near, float far)
     :  mNearPlane() {
 
     }
+
+    // =========================================================================
+
+    mq::mat4 Projection::perspective(float fovy, float aspect, float near, float far)
+    {
+        /*
+        | scale 0       0                           0  |
+        | 0     scale   0                           0  |
+        | 0     0       -far / (far - near)         -1 |
+        | 0     0       -far * near / (far - near)  0  |
+        */
+        const float scale = 1 / tan(fovy * 0.5f);
+        mq::mat4 P;
+        P[0][0] = scale; // scale x-coord of projected point
+        P[1][1] = scale; // scale y-corod of projected point
+        const float temp = -far / (far - near);
+        P[2][2] = temp; // remap x to [0, 1]
+        P[3][2] = near * temp; // remap x to [0, 1]
+        P[2][3] = -1;
+
+        return P;
+    }
+
+    // =========================================================================
 
     PerspectiveCamera::PerspectiveCamera(std::shared_ptr<Viewport> viewport,
         const vec3& origin, const vec3& lookAt, float fov,
